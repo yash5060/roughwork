897bccab25c2c8e50e55ebc07a3b875ae2a2f82a
Yash <98175266+yash5060@users.noreply.github.com>
2024-04-12 10:28:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Navbar from './Components/Start';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  );
+
+}
+
+export default App;
---
+++ Start.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Start.css'; // Import your CSS file for styling
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-title">kitab.com</div>
+      <div className="navbar-divider"></div>
+      <div className="navbar-divider"></div>
+      <div className="navbar-divider"></div>
+    </div>
+  );
+};
+
+export default Navbar;
